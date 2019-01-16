c5bfecc7fd188609dc9c0eee6db1bf9c9d33c9c6
Valentin Lieberknecht <me@valentinl.de>
2019-01-16 12:08:31 +0100

    [fix] calc age

--- js/main.js
+++ js/main.js
@@ -4,33 +4,47 @@ function loadImage(id, targetId) {
   var imageToLoad;
   if (el.dataset.image) {
     imageToLoad = el.dataset.image;
-  } else if (typeof el.currentSrc === 'undefined') {
+  } else if (typeof el.currentSrc === "undefined") {
     imageToLoad = el.src;
   } else {
     imageToLoad = el.currentSrc;
   }
   if (imageToLoad) {
-    var img = new Image();;
+    var img = new Image();
     img.src = imageToLoad;
     img.onload = function() {
-      targetEl.classList.add('is-loaded');
+      targetEl.classList.add("is-loaded");
     };
   }
 }
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   text();
-  loadImage('wallpaper');
-  loadImage('pictureImage', 'picture');
+  loadImage("wallpaper");
+  loadImage("pictureImage", "picture");
 });
 
 function text() {
-	var bday = 2000
-  var today = new Date()
-  var age = today.getFullYear() - bday
+  var age = getAge("2000/12/12");
+  var today = new Date();
 
-$.getJSON("https://ferien-api.de/api/v1/holidays/BB"), function(data) {
-  console.log(data);
+  $.getJSON("https://ferien-api.de/api/v1/holidays/BB"),
+    function(data) {
+      console.log(data);
+    };
+
+  document.getElementById("text").innerHTML =
+    "Hello, I am a " +
+    age +
+    " years old Student from Potsdam, Germany. Currently I am in the 12th grade and doing my A-level.";
 }
 
-  document.getElementById('text').innerHTML = "Hello, I am a " + age + " years old Student from Potsdam, Germany. Currently I am in the 12th grade and doing my A-level.";
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
 }
